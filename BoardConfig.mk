63a48b8c83f99bd8660f2102c3bdfebd5d777901
Cıyanogen <ttbpccorporation@outlook.com>
2019-01-06 20:03:16 +0300

    Initial Commit

---
+++ BoardConfig.mk
@@ -0,0 +1,65 @@
+#
+# Copyright (C) 2018 Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_TREE := device/samsung/j1xlte
+
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := universal3475
+
+# Platform
+BOARD_VENDOR := samsung
+TARGET_BOARD_PLATFORM := exynos3
+TARGET_SOC := exynos3475
+TARGET_BOOTLOADER_BOARD_NAME := universal3475
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+
+# Kernel
+TARGET_PREBUILT_KERNEL := device/samsung/j1xlte/kernel
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt device/samsung/j1xlte/dt.img
+
+# File systems
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# TWRP (not compatible with user build variant)
+RECOVERY_VARIANT := twrp
+TW_THEME := portrait_hdpi
+DEVICE_RESOLUTION := 480x800
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
+TW_BRIGHTNESS_PATH := /sys/devices/14800000.dsim/backlight/panel/brightness
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_INCLUDE_NTFS_3G := true
+TW_INCLUDE_CRYPTO := true
+TWRP_INCLUDE_LOGCAT := true
+TW_NO_LEGACY_PROPS := true
+TARGET_USERIMAGES_USE_EXT4 := true
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TARGET_RECOVERY_PIXEL_FORMAT := "ABGR_8888"
+TW_MAX_BRIGHTNESS := 255
+TW_EXCLUDE_SUPERSU := true
